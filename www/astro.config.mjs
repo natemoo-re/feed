f95abc6347fa490864adebfa9a184d135b778963
Nate Moore <nate@astro.build>
2022-11-06 12:35:48 -0600

    feat: add rss, single post pages

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://feed.nmoo.dev',
   vite: {
     ssr: {
       noExternal: ["he", "ultrahtml"],
---
+++ www/src/pages/rss.xml.ts
@@ -0,0 +1,15 @@
+import rss from '@astrojs/rss';
+import { getPosts } from '../utils/data';
+
+const posts = await getPosts()
+
+export const get = () => rss({
+  title: 'Nate Moore\'s Feed',
+  description: 'Short-form thoughts from Nate',
+  site: import.meta.env.SITE,
+  items: posts.map(post => ({
+    link: new URL(`/p/${post.name}`, import.meta.env.SITE).toString(),
+    title: post.data.title,
+    pubDate: post.data.date,
+  })),
+});
