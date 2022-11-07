5dba4dbc8a5737ae91ee0b4a5b409a94d76b9fdc
Nate Moore <nate@astro.build>
2022-11-07 07:49:10 -0600

    ci

--- .github/actions/parse/src/index.ts
+++ .github/actions/parse/src/index.ts
@@ -1,6 +1,16 @@
 import { getInput } from '@actions/core';
 import fs from 'node:fs';
 import path from 'node:path';
+import { remark } from 'remark'
+import strip from 'strip-markdown';
+
+const formatTweet = async (markdown: string) => {
+  const body = remark()
+    .use(strip)
+    .process(markdown)
+    .then((file) => String(file))
+  console.log(body);
+}
 
 async function run() {
     const files = getInput('files');
@@ -8,8 +18,11 @@ async function run() {
         if (!file.endsWith('.md')) return;
         const contents = fs.readFileSync(file, { encoding: 'utf-8' });
         const id = path.basename(file).replace('.md', '');
-        const post = contents.split('---').slice(1);
-        console.log({ id, post });
+        const [markdown] = contents.split('---').slice(2);
+
+        const url = `feed.nmoo.dev/p/${id}`;
+        const body = await formatTweet(markdown);
+        console.log({ url, body });
     }
 }
 
